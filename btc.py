460e47a7a00e4a5348776e0d6ecefb8827ae4ada
Ricardo <rcarrera98@gmail.com>
2021-02-09 21:11:48 -0500

    coingecko api

--- btc.py
+++ btc.py
@@ -1,11 +1,7 @@
-usd = 250
-btc_price_at_buy = 35000
-btc_actual_price = 40000
-new_usd = (usd * btc_actual_price) / btc_price_at_buy
-new_usd = round(new_usd, 2)
-print(new_usd)
+import requests
+import json
 
-goal_usd = 1000
-btc_price_to_goal = (goal_usd * btc_price_at_buy) / usd
-btc_price_to_goal = round(btc_price_to_goal, 2)
-print(btc_price_to_goal)
+r = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
+bitcoin = json.loads(r.text)
+price = bitcoin['bitcoin']['usd']
+print('Bitcoin current price is: ${}'.format(price))
