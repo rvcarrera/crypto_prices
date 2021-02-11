14b946bae12a5a69f77fdc6f29c89d346b5f635a
Ricardo <rcarrera98@gmail.com>
2021-02-11 15:42:21 -0500

    notebook btc

--- btc.py
+++ btc.py
@@ -1,7 +1,20 @@
 import requests
 import json
+import numpy as np
+import pandas as pd
+import time
 
-r = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
-bitcoin = json.loads(r.text)
-price = bitcoin['bitcoin']['usd']
-print('Bitcoin current price is: ${}'.format(price))
+date = int(time.time())
+a_year_before = date - 31556926
+
+btc_request = f'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=usd&from={a_year_before}&to={date}'
+
+r = requests.get(btc_request)
+
+database = r.json()
+
+prices = np.array(database['prices'])
+
+price_series = pd.Series(prices[:][1])
+
+print(price_series)
